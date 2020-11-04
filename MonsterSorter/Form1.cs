2494177926738ac26c6cb42ee0a3cef1977939ed
caitgunn520@outlook.com <caitgunn520@outlook.com>
2020-11-04 14:40:10 -0500

    asdfasd

--- MonsterSorter/Form1.cs
+++ MonsterSorter/Form1.cs
@@ -7,22 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace MonsterSorter
 {
     public partial class Form1 : Form
     {
         List<Monster> monsterList = new List<Monster>();
+        string typeShown; //Used for Show all [x] buttons
 
         public Form1()
         {
             InitializeComponent();
-            //Read xml file “Saved Monsters”
-            //For each 2 lines in xml file “Saved Monsters”
-            //Create new monster
-            //Name = line 1
-            //Type = line 2
-            //Add new monster to global list “Monsters”
+            List<string> savedList = File.ReadAllLines("SavedMonsters.txt").ToList();
+
+            for (int i = 0; i < savedList.Count; i += 2)
+            {
+                string name = savedList[i];
+                string type = savedList[i + 1];
+
+                Monster newMon = new Monster(name, type);
+                monsterList.Add(newMon);
+            }
         }
 
         private void addMonsterButton_Click(object sender, EventArgs e)
@@ -36,6 +42,7 @@ namespace MonsterSorter
 
                 Monster newMon = new Monster(monName, monType);
                 monsterList.Add(newMon);
+                monsterOutput.Text = $"{monName} successfully added.";
             }
             else
             {
@@ -65,51 +72,63 @@ namespace MonsterSorter
 
             if (index >= 0)
             {
-                if ()
+                if (monsterList[index].type != typeInput.SelectedItem.ToString())
                 {
-
+                    monsterList[index].type = typeInput.SelectedItem.ToString();
+                }
+                else
+                {
+                    monsterOutput.Text = "Monster is already that type.";
                 }
             }
-            
-            //TODO If monster with same name as input in name box exists
-            //and If monster has different type than input in type box
-            //Change monster type to input in type box
-            //Else
-            //Return error message - “Monster is already that type”
         }
 
         private void showAllButton_Click(object sender, EventArgs e)
         {
-            //For each object in global list “Monsters”
-            //Display object on output screen
+            monsterOutput.Text = "";
+
+            for (int i = 0; i < monsterList.Count(); i++)
+            {
+                monsterOutput.Text += $"{monsterList[i].name}\n{monsterList[i].type}\n";
+            }
         }
 
         private void showEarth_Click(object sender, EventArgs e)
         {
-            //For each object in global list “Monsters”
-            //If type = [x]
-            //Display object on output screen
+            typeShown = "Earth";
+            ShowAllType();
         }
 
         private void showWater_Click(object sender, EventArgs e)
         {
-            //For each object in global list “Monsters”
-            //If type = [x]
-            //Display object on output screen
+            typeShown = "Water";
+            ShowAllType();
         }
 
         private void showFire_Click(object sender, EventArgs e)
         {
-            //For each object in global list “Monsters”
-            //If type = [x]
-            //Display object on output screen
+            typeShown = "Fire";
+            ShowAllType();
         }
 
         private void showAir_Click(object sender, EventArgs e)
         {
-            //For each object in global list “Monsters”
-            //If type = [x]
-            //Display object on output screen
+            typeShown = "Air";
+            ShowAllType();
+        }
+
+        public void ShowAllType()
+        {
+            monsterOutput.Text = "";
+            
+            for (int i = 0; i < monsterList.Count(); i++)
+            {
+                if (monsterList[i].type == typeShown)
+                {
+                    monsterOutput.Text += $"{monsterList[i].name}\n{monsterList[i].type}" +
+                        $"\n";
+                }
+            }
         }
 
         private void searchButton_Click(object sender, EventArgs e)
@@ -119,11 +138,26 @@ namespace MonsterSorter
             //Display monster attributes in output
             //Else
             //Return error message - “Monster not found”
+            int index = monsterList.FindIndex(n => n.name == nameInput.Text);
+
+            if (index >= 0)
+            {
+                monsterOutput.Text = $"{monsterList[index].name}\n{monsterList[index].type}";
+            }
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
             //TODO Save all objects in global list “Monsters” to xml file “Saved Monsters”
+            List<string> saveList = new List<string>();
+
+            foreach (Monster mon in monsterList)
+            {
+                saveList.Add(mon.name);
+                saveList.Add(mon.type);
+            }
+
+            File.WriteAllLines("SavedMonsters.txt", saveList);
         }
     }
 }
